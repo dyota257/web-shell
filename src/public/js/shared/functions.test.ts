1abd0bee4fcace391f76abb3443124d47b53df14
Dyota Tanuwibawa <dyota257@gmail.com>
2022-01-12 21:21:28 +0800

    time zone offset in getCurrentTime

---
+++ src/public/js/shared/functions.test.ts
@@ -0,0 +1,8 @@
+import { getCurrentTime } from './functions';
+
+describe('getCurrentTime', () =>{
+    it('gets the current time in the right timezone', () => {
+
+        expect(true).toBe(true);
+    });
+});
--- src/public/js/shared/functions.ts
+++ src/public/js/shared/functions.ts
@@ -1,7 +1,9 @@
 export function getCurrentTime() {
     let today = new Date();
+    let offset = today.getTimezoneOffset()/60 // GMT +8 returns -8
+
     return (
-        today.getHours().toString().padStart(2, '0') +
+        (today.getHours() - offset).toString().padStart(2, '0') +
         ':' +
         today.getMinutes().toString().padStart(2, '0')
     );
